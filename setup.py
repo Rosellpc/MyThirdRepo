fbff50674042031fecb613bb600e30eefca5ffaf
Hacker Rosell <rossellpc@gmail.com>
2026-03-18 22:21:35 -0500

    The last change in the pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tercerrepo",  # Nombre del paquete
-    version="0.1.0",  # Versión inicial
+    version="0.1.1",  # Versión inicial
     packages=find_packages(),  # Paquetes a incluir
     description="Un paquete pip simple de saludo",  # Breve descripción
     author="RosellPC",  # Tu nombre
