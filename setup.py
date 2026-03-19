fa7d1139b1295dd6e6a23614eeb717acaa5ee7a4
Hacker Rosell <rossellpc@gmail.com>
2026-03-18 22:49:09 -0500

    The  last change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="tercerrepo",  # Nombre del paquete
+    name="MyThirdRepo",  # Nombre del paquete
     version="0.1.1",  # Versión inicial
     packages=find_packages(),  # Paquetes a incluir
     description="Un paquete pip simple de saludo",  # Breve descripción
